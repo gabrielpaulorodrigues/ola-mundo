9a18efde68b2fa653111d1747b0899763fbd87f2
Gabriel Paulo Rodrigues <gabrielpaulogpsc@gmail.com>
2024-10-16 20:40:35 -0300

    adicionando paginas

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,22 +1,17 @@
-import { Link} from 'react-router-dom'
 import styles from './Menu.module.css'
 import MenuLink from '../MenuLink'
 
 export default function Menu(){
-    
-    
-
-    console.log(localizacao)
 
     return (
         <header>
             <nav className={styles.navegacao}>
-                <MenuLink>
+                <MenuLink to='/'>
                     Inicio
                 </MenuLink>
-                <Link className={styles.link} to='/sobremim'>
-                    Sobre mim 
-                </Link>
+                <MenuLink to='/sobremim'>
+                    Sobre Mim
+                </MenuLink>
             </nav>
         </header>
     )
--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,5 +1,11 @@
+import Banner from "componentes/banner";
+
 export default function Inicio(){
     return (
+        <main>
+        <Banner/>
+
         <h1>Olá Mundo</h1>
+        </main>
     )
 }
